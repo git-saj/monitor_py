8db9c6b481cf8cb96938db75f44f6be71ac0f7f6
git-saj <salimyasser@icloud.com>
2020-12-18 09:21:49 +0000

    Added better output logging for check

--- monitor_check.py
+++ monitor_check.py
@@ -2,8 +2,12 @@ import cosmosdb
 import monitor_smtp
 import requests
 import json
+import datetime
+now = datetime.datetime.now()
 
 def request_sites():
+
+    check_int = 0
 
     monitor_sites = json.loads(cosmosdb.select_monitor_sites())
     
@@ -18,7 +22,13 @@ def request_sites():
         site_status_code = request.status_code
 
         if int(site_status_code) != int(site_expected_status):
+            check_int = 1
             monitor_smtp.monitor_sendmail(site_group, site_url, site_expected_status, site_status_code)
+
+    if check_int == 0:
+        output_str = "%s: %s sites checked and all returned expected status codes" % (now, len(monitor_sites))
+        print(output_str)
+
 
 if __name__ == "__main__":
     request_sites()
